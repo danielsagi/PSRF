7629d942e593023d51938c93e676d0869a3189c2
danielsagi <danielsagi2009@gmail.com>
2018-09-05 01:07:25 +0300

    fixed bug in solver filter function

--- solver/app.py
+++ solver/app.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import json
 import requests
+import logging
 from flask import Flask, request
 app = Flask(__name__)
 
@@ -10,9 +11,9 @@ def get_captain_ip():
 	node_ip="10.132.0.2"
 	try:
 		pods = json.loads(requests.get("http://{}/pods".format(node_ip)).text)["items"]
-		return filter(lambda x: x["metadata"]["name"] == "captain", pods)["status"]["podIP"]
+		return filter(lambda x: x["metadata"]["name"] == "captain", pods)[0]["status"]["podIP"]
 	except Exception as x:
-		print "Failed getting captain ip: {}".format(x.message)
+		logging.error("Failed getting captain ip: {}".format(x.message))
 		
 @app.route('/')
 @app.route('/flag') # append something for addess to send the flag to
@@ -29,7 +30,7 @@ def PSRF():
 				except:
 					return "give me a *valid* webhook\n", 500
 			else:
-				data = "pass me a webhook please\n"
+				data = "pass me a ?webhook= please\n"
 		else:
 			data = "go to /flag!!!\n"
 	else:
